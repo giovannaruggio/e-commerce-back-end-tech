de339d3a9b776e0ed02ad1e9cbcf439ff744d8e3
Giovanna Ruggio <giovannaruggio@gmail.com>
2022-02-19 16:17:01 -0500

    Beginning of models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tags',
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'products',
+  foreignKey: 'tag_id'
+});
+
+// ProductTags belongsTo Tags
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
